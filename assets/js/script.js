f3872e32343d63bb3af5238bf800f14e5f1bfa09
sm3131 <sm2683@nau.edu>
2021-11-18 21:50:52 -0700

    created begin quiz function that starts timer on start button click

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,13 +3,13 @@ var startQuizEl = document.querySelector(".start");
 var quizQuestionsEl = document.querySelector(".questions");
 var finishQuizEl = document.querySelector(".finish");
 var highScoreEl = document.querySelector(".highscore");
+var timerEl = document.querySelector("#timer");
 
 startQuiz();
 
 // initial start quiz element creation
 function startQuiz() {
         var startH2El = document.createElement("h2");
-        startH2El.className = "title"
         startH2El.textContent = "Code Quiz Challenge!";
         startQuizEl.appendChild(startH2El);
 
@@ -27,11 +27,36 @@ function startQuiz() {
 function beginQuiz() {
     startQuizEl.textContent = "";
     // Start Timer
+    timeStart();
+    //questionElCreator();
+
+    
     // Call Question creator function
     // Call Question Elements creator
     // Call Element content addition
 }
 
+function timeStart() {
+    var timeLeft = 120;
+    var timer = setInterval(function() {
+                timerEl.textContent = "Time: " + timeLeft;
+                timeLeft--;
+                if (timeLeft === -1) {
+                    clearInterval(timer);
+                }
+    }, 1000);
+}
+
+
+
+// check button if/else:
+// if (button clicked) {
+//     run check answer function
+//     next question function
+// } else {
+//     check timer function
+//     If (timer <= 0) { run finish quiz function}
+//     else { run button listener function above}
 
 // Create Question Element Creator function
 /* <h2 class="ask">Question</h2>
