462197591bbde86f37227deeb3279e3ab5b391dc
sm3131 <sm2683@nau.edu>
2021-11-22 18:24:48 -0700

    split create questions function into two functions, added if/else to check answer functions, added a hard coded html element

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,9 @@
 // creating variables that target specific html elements
 var startQuizEl = document.querySelector(".start");
+// var questionsGroup = document.querySelector(".questions-div");
 var quizQuestionsEl = document.querySelector(".questions");
 var quizQuestionsListEl = document.querySelector(".questions-list");
+var accuracyEl = document.querySelector(".accuracy");
 var finishQuizEl = document.querySelector(".finish");
 var highScoreEl = document.querySelector(".highscore");
 var timerEl = document.querySelector("#timer");
@@ -51,119 +53,147 @@ function timeStart() {
 
 // function to create question elements
 function questionElCreator() {
-    for (var i = 0; i < 1; i++) {
+
+    questionEl = document.createElement("h2");
+    //questionEl.textContent = test[i].ask
+    quizQuestionsEl.appendChild(questionEl);
+
+    answer1El = document.createElement("li");
+    //answer1El.textContent = test[i].answer1;
+    answer1El.className = "ans1";
+    quizQuestionsListEl.appendChild(answer1El);
+
+    answer2El = document.createElement("li");
+    // answer2El.textContent = test[i].answer2;
+    answer2El.className = "ans2";
+    quizQuestionsListEl.appendChild(answer2El);
+
+    answer3El = document.createElement("li");
+    //answer3El.textContent = test[i].answer3
+    answer3El.className = "ans3";
+    quizQuestionsListEl.appendChild(answer3El);
+
+    answer4El = document.createElement("li");
+    //answer4El.textContent = test[i].answer4
+    answer4El.className = "ans4";
+    quizQuestionsListEl.appendChild(answer4El);
+
+    
+    questionContentAddition();
+}
+
+
+    function questionContentAddition() {
+    for (var i = 0; i < 2; i++) {
         globalI = i;
         if (timeLeft === 0) {
             break;
         } else {
-
-            var questionEl = document.createElement("h2");
             questionEl.textContent = test[i].ask
-            quizQuestionsEl.appendChild(questionEl);
-
-            var answer1El = document.createElement("li");
             answer1El.textContent = test[i].answer1;
-            answer1El.className = "ans1"
-            quizQuestionsListEl.appendChild(answer1El);
-
-            var answer2El = document.createElement("li");
             answer2El.textContent = test[i].answer2;
-            answer2El.className = "ans2"
-            quizQuestionsListEl.appendChild(answer2El);
-
-            var answer3El = document.createElement("li");
-            answer3El.textContent = test[i].answer3
-            answer3El.className = "ans3"
-            quizQuestionsListEl.appendChild(answer3El);
-
-            var answer4El = document.createElement("li");
-            answer4El.textContent = test[i].answer4
-            answer4El.className = "ans4"
-            quizQuestionsListEl.appendChild(answer4El);
-
-
-            quizQuestionsListEl.addEventListener("click", function (event) {
-                targetEl = event.target;
-
-                if (targetEl.matches(".ans1")) {
-                    checkAns1();
-                } else if (targetEl.matches(".ans2")) {
-                    checkAns2();
-                } else if (targetEl.matches(".ans3")) {
-                    checkAns3();
-                } else if (targetEl.matches(".ans4")) {
-                    checkAns4();
-                }
-
-            });
+            answer3El.textContent = test[i].answer3;
+            answer4El.textContent = test[i].answer4;
         }
+        
+        quizQuestionsListEl.addEventListener("click", function (event) {
+            // debugger;
+            targetEl = event.target;
+            if (targetEl.matches(".ans1")) {
+                checkAns1();
+            } else if (targetEl.matches(".ans2")) {
+                checkAns2();
+            } else if (targetEl.matches(".ans3")) {
+                checkAns3();
+            } else if (targetEl.matches(".ans4")) {
+                checkAns4();
+            }
+            quizQuestionsEl.textContent = "";
+            quizQuestionsListEl.textContent = "";
+        });
+    }
+};
+
+
+function checkAns1() {
+    quizQuestionsEl.textContent = "";
+    quizQuestionsListEl.textContent = "";
+    if (test[globalI].answer1 === test[globalI].correct) {
+        var correctEl = document.createElement("h3");
+        correctEl.textContent = "Correct!";
+        accuracyEl.appendChild(correctEl);
+        console.log("correct")
+    } else {
+        var incorrectEl = document.createElement("h3");
+        incorrectEl.textContent = "Incorrect!";
+        accuracyEl.appendChild(incorrectEl);
+        console.log("incorrect")
     }
 }
 
-    function checkAns1() {
-        if (test[globalI].answer1 === test[globalI].correct) {
-            console.log("correct")
-        } else {
-            console.log("incorrect")
-        }
+function checkAns2() {
+    if (test[globalI].answer2 === test[globalI].correct) {
+        var correctEl = document.createElement("h3");
+        correctEl.textContent = "Correct!";
+        accuracyEl.appendChild(correctEl);
+        console.log("correct")
+    } else {
+        var incorrectEl = document.createElement("h3");
+        incorrectEl.textContent = "Incorrect!";
+        accuracyEl.appendChild(incorrectEl);
+        console.log("incorrect")
     }
+}
 
-    function checkAns2() {
-        if (test[globalI].answer2 === test[globalI].correct) {
-            console.log("correct")
-        } else {
-            console.log("incorrect")
-        }
+function checkAns3() {
+    if (test[globalI].answer3 === test[globalI].correct) {
+        var correctEl = document.createElement("h3");
+        correctEl.textContent = "Correct!";
+        accuracyEl.appendChild(correctEl);
+        console.log("correct")
+    } else {
+        var incorrectEl = document.createElement("h3");
+        incorrectEl.textContent = "Incorrect!";
+        accuracyEl.appendChild(incorrectEl);
+        console.log("incorrect")
     }
+}
 
-    function checkAns3() {
-        if (test[globalI].answer3 === test[globalI].correct) {
-            console.log("correct")
-        } else {
-            console.log("incorrect")
-        }
+function checkAns4() {
+    if (test[globalI].answer4 === test[globalI].correct) {
+        var correctEl = document.createElement("h3");
+        correctEl.textContent = "Correct!";
+        accuracyEl.appendChild(correctEl);
+        console.log("correct")
+    } else {
+        var incorrectEl = document.createElement("h3");
+        incorrectEl.textContent = "Incorrect!";
+        accuracyEl.appendChild(incorrectEl);
+        console.log("incorrect")
     }
+}
 
-    function checkAns4() {
-        if (test[globalI].answer4 === test[globalI].correct) {
-            console.log("correct")
-        } else {
-            console.log("incorrect")
-        }
+
+var test = [
+    {
+        "ask": "Question1",
+        "answer1": "Q1Answer1",
+        "answer2": "Q1Answer2",
+        "answer3": "Q1Answer3",
+        "answer4": "Q1Answer4",
+        "correct": "Q1Answer1"
+    },
+    {
+        "ask": "Question2",
+        "answer1": "Q2Answer1",
+        "answer2": "Q2Answer2",
+        "answer3": "Q2Answer3",
+        "answer4": "Q2Answer4",
+        "correct": "Q2Answer2"
     }
+];
 
-    // answerEl.addEventListener("click", checkAns(test[i].answer1));
-    // answerEl.addEventListener("click", checkAns(test[i].answer2));
-    // answerEl.addEventListener("click", checkAns(test[i].answer3));
-    // answerEl.addEventListener("click", checkAns(test[i].answer4));
 
-    // function checkAns(answerPara) {
-    //     if (answerPara === test[i].correct) {
-    //         console.log("correct");
-    //     } else {
-    //         console.log("incorrect");
-    //     }
-
-    // }
-
-    var test = [
-        {
-            "ask": "Question1",
-            "answer1": "Answer1",
-            "answer2": "Answer2",
-            "answer3": "Answer3",
-            "answer4": "Answer4",
-            "correct": "Answer1"
-        },
-        // {
-        //     "ask": "Question2",
-        //     "answer1": "Answer1",
-        //     "answer2": "Answer2",
-        //     "answer3": "Answer3",
-        //     "answer4": "Answer4",
-        //     "correct": "Answer1"
-        // }
-    ];
 // check button if/else:
 // if (button clicked) {
 //     run check answer function
